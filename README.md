686f864784be00fad23cbfe12e4ca456bfb624ce
alkari <al@alkari.com>
2017-12-07 14:46:26 -0800

    upload script

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # install_cv2
-Install OpenCV 3.3
+###Install OpenCV 3.3
+
+Script to download and compile OpenCV 3.3 with ffmegp
+
+Usage...
+
+## Ubuntu 16.04 LTS
+
+'''
+cd ~
+https://github.com/alkari/install_cv2.git
+chmod +x ubuntu_install_cv2.sh
+sudo ./ubuntu_install_cv2.sh
+'''
+
+
---
+++ ubuntu_install_cv2.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#
+# Compile OpenCV 3.3 with ffmpeg
+#
+# Tested on Ubuntu 16.04 LTS
+#
+# Author: @alkari
+
+set -e
+
+apt-get -y update
+
+apt-get -y install python3.5-dev python3-pip wget unzip \
+    build-essential cmake git pkg-config libatlas-base-dev gfortran \
+    libjasper-dev libgtk2.0-dev libavcodec-dev libavformat-dev \
+    libswscale-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev \
+    libv4l-dev ffmpeg
+
+pip3 install --upgrade pip
+pip3 install --upgrade scikit-image
+
+pip3 install numpy
+
+cd ~
+
+wget https://github.com/Itseez/opencv/archive/3.3.0.zip -O opencv3.zip
+
+unzip -q opencv3.zip && mv opencv-3.3.0 opencv && rm opencv3.zip
+
+wget https://github.com/Itseez/opencv_contrib/archive/3.3.0.zip -O opencv_contrib3.zip
+
+unzip -q opencv_contrib3.zip && mv opencv_contrib-3.3.0 opencv_contrib && rm opencv_contrib3.zip
+
+mkdir opencv/build && cd opencv/build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+      -D BUILD_PYTHON_SUPPORT=ON 
+      -D CMAKE_INSTALL_PREFIX=/usr/local \
+      -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules 
+      -D BUILD_EXAMPLES=OFF 
+      -D PYTHON_DEFAULT_EXECUTABLE=/usr/bin/python3 \
+      -D BUILD_opencv_python3=ON \
+      -D BUILD_opencv_python2=OFF \
+      -D WITH_IPP=OFF \
+      -D WITH_FFMPEG=ON \
+      -D WITH_V4L=ON .. 
+
+make -j$(nproc)
+
+make install
+
+ldconfig
+
+apt-get -y remove build-essential cmake git pkg-config libatlas-base-dev \
+    gfortran libjasper-dev libgtk2.0-dev libavcodec-dev libavformat-dev \
+    libswscale-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libv4l-dev
+
+apt-get clean
+
+rm -rf /var/lib/apt/lists/*
+
+cd ~
+
+rm -rf opencv opencv_contrib
+
+
+
+
